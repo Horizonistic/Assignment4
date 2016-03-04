dee8572c5f8669fb70abfd9977f7557f02a78a3c
Infernia <horizonistic@gmail.com>
2016-03-03 18:00:15 -0800

    Initial commit

---
+++ src/Bikes.cpp
@@ -0,0 +1,55 @@
+//
+// Created by Infernia on 03/03/2016.
+//
+
+#include "Bikes.h"
+
+void Bikes::readBike(fstream &, Bike*& head, Bike*& manuf, Bike*& id)
+{
+    // TODO
+}
+
+Bike * Bikes::addBike(Bike* head, Bike* new_bike)
+{
+    // TODO
+}
+
+Bike * Bikes::addBike(Bike * head_id, Bike * new_bike, order)
+{
+    // TODO
+}
+
+Bike * Bikes::addBike(Bike * head_manufacturer, Bike * new_bike, order, int)
+{
+    // TODO
+}
+
+void Bikes::printBikes(Bike*, order, status)
+{
+    // TODO
+}
+
+void Bikes::doTransaction(fstream &, Bike*, status)
+{
+    // TODO
+}
+
+void Bikes::deleteIdBike(fstream &, Bike*)
+{
+    // TODO
+}
+
+void Bikes::deleteManufacuter(fstream &, Bike*)
+{
+    // TODO
+}
+
+void Bikes::deleteBikes(Bike *)
+{
+    // TODO
+}
+
+void Bikes::init(Bike*)
+{
+    // TODO
+}
